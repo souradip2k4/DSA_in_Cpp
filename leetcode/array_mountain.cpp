a7ffff5a5c3738adc521ba8648570f4a6904602b
souradipdev <22052939@kiit.ac.in>
2024-08-19 20:59:16 +0530

    Added mounted array code

---
+++ leetcode/array_mountain.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int> &arr) {
+      int start = 0, end = arr.size() - 1, index = 0;
+      int mid = start + (end - start) / 2;
+
+      while (end > start) {
+        if (arr[mid] < arr[mid + 1]) {
+          start = mid + 1;
+          index = start;
+        } else if (arr[mid] > arr[mid + 1]) {
+          end = mid;
+        }
+
+
+        mid = start + (end - start) / 2;
+      }
+
+      return index;
+    }
+};
+
+// start = 0, 1, 2, 3
+// mid = 1, 2, 2, 3
+
+int main() {
+  Solution soln;
+  vector<int> arr = {0, 2, 4, 6};
+  int result = soln.peakIndexInMountainArray(arr);
+
+  cout << "Index of largest element in array: " << result << endl;
+  return 0;
+}
