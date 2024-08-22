d5cad0ef02de97a067180033dfaf7a3d040a60d7
souradipdev <22052939@kiit.ac.in>
2024-08-22 18:51:49 +0530

    Added quick sort code

---
+++ coding_ninja/min_dishes.cpp
@@ -0,0 +1,16 @@
+// https://www.naukri.com/code360/problems/minimum-dishes_2280380
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minimumDishes(int n, int m, vector<int> arr) {
+
+
+}
+
+int main() {
+  vector<int> arr = {1, 2, 3, 1, 1, 2};
+
+  int minDishes = minimumDishes(6, 2, arr);
+}
