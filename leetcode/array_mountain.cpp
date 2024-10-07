e5b93c8c91888eae818ddccdc9cc0f0a6a6f4856
souradipdev <22052939@kiit.ac.in>
2024-10-08 02:33:55 +0530

    Modified array_mountain code

--- leetcode/array_mountain.cpp
+++ leetcode/array_mountain.cpp
@@ -1,27 +1,38 @@
 // https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int> &arr) {
-      int start = 0, end = arr.size() - 1, index = 0;
-      int mid = start + (end - start) / 2;
+      int l = 0, h = arr.size() - 1;
+      int n = arr.size();
+      int mid = l + (h - l) / 2;
 
-      while (end > start) {
-        if (arr[mid] < arr[mid + 1]) {
-          start = mid + 1;
-          index = start;
-        } else if (arr[mid] > arr[mid + 1]) {
-          end = mid;
-        }
-
-
-        mid = start + (end - start) / 2;
+      if (n == 1) {
+        return 0;
+      } else if (arr[0] > arr[1]) {
+        return 0;
+      } else if (arr[n - 1] > arr[n - 2]) {
+        return n - 1;
       }
 
-      return index;
+      while (l <= h) {
+
+        if (mid - 1 >= 0 && mid + 1 < n && arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+          return mid;
+        } else if (mid - 1 >= 0 && arr[mid - 1] > arr[mid]) {
+          h = mid - 1;
+        } else if (mid + 1 < n && arr[mid] < arr[mid + 1]) {
+          l = mid + 1;
+        }
+
+        mid = l + (h - l) / 2;
+      }
+
+      return -1;
     }
 };
 
@@ -30,7 +41,7 @@ public:
 
 int main() {
   Solution soln;
-  vector<int> arr = {0, 2, 4, 6};
+  vector<int> arr = {3, 5, 3, 2, 0};
   int result = soln.peakIndexInMountainArray(arr);
 
   cout << "Index of largest element in array: " << result << endl;
