61b00e51994c119269439afad0add7837b2c4e6b
souradipdev <22052939@kiit.ac.in>
2024-10-15 21:59:18 +0530

    Added ship_package_capacity code

--- leetcode/min_days_m_bouquets.cpp
+++ leetcode/min_days_m_bouquets.cpp
@@ -35,12 +35,13 @@ int minDays(vector<int> &bloomDay, int m, int k) {
     maxElem = max(maxElem, elem);
   }
 
-
   int l = minElem, h = maxElem;
+  int result = h;
   int mid = l + (h - l) / 2;
 
   while (l < h) {
     if (possible(bloomDay, mid, k, m)) {
+      result = mid;
       h = mid - 1;
     } else {
       l = mid + 1;
@@ -49,14 +50,14 @@ int minDays(vector<int> &bloomDay, int m, int k) {
     mid = l + (h - l) / 2;
   }
 
-  return mid;
+  return result;
 }
 
 int main() {
   vector<int> arr1 = {7, 7, 7, 7, 12, 7, 7}; // m = 2  k = 3
   vector<int> arr2 = {1, 10, 3, 10, 2}; // m = 3  k = 2
 
-  int min = minDays(arr2, 3, 1);
+  int min = minDays(arr1, 2, 3);
 
   cout << min << endl;
 }
---
+++ leetcode/ship_package_capacity.cpp
@@ -0,0 +1,55 @@
+//  https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int calculatedDays(vector<int> &weights, int mid, int days) {
+
+  int count = 1, calcSum = 0;
+  for (int i = 0; i < weights.size(); i++) {
+
+    if (calcSum + weights[i] > mid) {
+      count++;
+      calcSum = weights[i];
+    } else {
+      calcSum += weights[i];
+    }
+  }
+
+//  cout << count << endl;
+  return count;
+}
+
+int shipWithinDays(vector<int> &weights, int days) {
+  int weightSum = 0;
+int result;
+  int maxElem = INT_MIN;
+  for (int elem: weights) {
+    maxElem = max(maxElem, elem);
+    weightSum += elem;
+  }
+
+  int l = maxElem, h = weightSum;
+
+  int mid = l + (h - l) / 2;
+  while (l <= h) {
+    if (calculatedDays(weights, mid, days) <= days) {
+      result = mid;
+      h = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+    mid = l + (h - l) / 2;
+  }
+
+  return result;
+}
+
+int main() {
+  vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // days = 5
+  vector<int> arr2 = {3, 2, 2, 4, 1, 4}; // days = 3
+
+  int minCapacity = shipWithinDays(arr1, 5);
+
+  cout << minCapacity << endl;
+}
