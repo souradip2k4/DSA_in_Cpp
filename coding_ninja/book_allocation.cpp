597cdb4b19879a316ce274070a2c4b022ba3d63e
souradipdev <22052939@kiit.ac.in>
2024-10-16 03:29:51 +0530

    Added book_allocation code

---
+++ coding_ninja/book_allocation.cpp
@@ -0,0 +1,56 @@
+// https://www.naukri.com/code360/problems/allocate-books_1090540
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int calculateStudents(vector<int> &arr, int mid) {
+  int sum = 0, count = 1;
+
+  for (int i = 0; i < arr.size(); i++) {
+    sum += arr[i];
+    if (sum  > mid) {
+      count++;
+      sum = arr[i];
+    }
+  }
+
+  return count;
+}
+
+int findPages(vector<int> &arr, int n, int m) {
+  int maxElem = INT_MIN, maxSum = 0;
+  if (m > n) {
+    return -1;
+  }
+
+  for (int elem: arr) {
+    maxElem = max(elem, maxElem);
+    maxSum += elem;
+  }
+
+  int l = maxElem, h = maxSum;
+  int result = h;
+  int mid = l + (h - l) / 2;
+
+  while (l <= h) {
+    if (calculateStudents(arr, mid) <= m) {
+      result = mid;
+      h = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+
+    mid = l + (h - l) / 2;
+  }
+
+  return result;
+}
+
+int main() {
+  vector<int> arr1 = {12, 34, 67, 90}; // m = 2
+  vector<int> arr2 = {25, 46, 28, 49, 24}; // m = 4
+
+  int minPages = findPages(arr1, arr1.size(), 2);
+
+  cout << minPages << endl;
+}
