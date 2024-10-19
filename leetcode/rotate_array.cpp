43335d933614a846203b3e2d09125ffea65fd34a
souradipdev <22052939@kiit.ac.in>
2024-10-20 01:23:40 +0530

    Some improvements rotate_array code

--- leetcode/rotate_array.cpp
+++ leetcode/rotate_array.cpp
@@ -6,22 +6,21 @@ using namespace std;
 
 void rotate(vector<int> &arr, int k) {
 
-  k = k % arr.size();
+  k = k % arr.size(); // if k > size of arr[] then reverse will be done on basis of this
   reverse(arr.begin(), arr.end());
   reverse(arr.begin(), arr.begin() + k);
   reverse(arr.begin() + k, arr.end());
 }
 
 int main() {
-  vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7};
-  vector<int> arr2 = {-1, -100, 3, 99};
+  vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7}; // k = 3
+  vector<int> arr2 = {-1, -100, 3}; // k = 4
 
-  rotate(arr1, 3);
+  rotate(arr2, 4);
 
-  for (int elem: arr1) {
+  for (int elem: arr2) {
     cout << elem << " ";
   }
 
   cout << endl;
-
 }
