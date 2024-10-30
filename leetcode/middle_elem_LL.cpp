e799edb5f804d6e932900dfe738b21c840d3086f
souradipdev <22052939@kiit.ac.in>
2024-10-30 23:57:34 +0530

    Added middle_elem_LL code

---
+++ leetcode/middle_elem_LL.cpp
@@ -0,0 +1,26 @@
+// https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+      ListNode *fastPtr = head, *slowPtr = head;
+
+      while(fastPtr != nullptr && fastPtr->next != nullptr){
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+      }
+
+      return slowPtr;
+    }
+};
