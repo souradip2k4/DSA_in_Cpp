f6f3283244ea5cf4dd01f591eb62e0aeb0bd8dd5
souradipdev <22052939@kiit.ac.in>
2024-11-02 23:01:30 +0530

    Added delete_mid_node_LL code

---
+++ leetcode/delete_mid_node_LL.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+#include <bits/stdc++.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *deleteMiddle(ListNode *head) {
+      if (head == nullptr || head->next == nullptr) {
+        return nullptr;
+      }
+
+      ListNode *slow = head, *fast = head->next;
+
+      while (fast->next != nullptr && fast->next->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+      }
+
+      ListNode *del = slow->next;
+      slow->next = slow->next->next;
+      delete del;
+      return head;
+    }
+};
