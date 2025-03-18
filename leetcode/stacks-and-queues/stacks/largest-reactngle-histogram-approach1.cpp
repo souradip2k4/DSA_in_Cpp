2bc080f7d2bbc6fa255c1a920d5777031132c2c8
souradip2k4 <22052939@kiit.ac.in>
2025-03-18 23:51:39 +0530

    Added largest-reactngle-histogram-approach1.cpp

---
+++ leetcode/stacks-and-queues/stacks/largest-reactngle-histogram-approach1.cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+// 84. Largest Rectangle in Histogram
+
+#include <values.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> prevSmallestElem(vector<int> &heights) {
+  stack<int> st;
+  int n = heights.size();
+  vector<int> pse(n);
+  for (int i = 0; i < n; i++) {
+    while (!st.empty() && heights[i] <= heights[st.top()]) {
+      st.pop();
+    }
+
+    if (!st.empty()) {
+      pse[i] = st.top();
+    } else {
+      pse[i] = -1;
+    }
+
+    st.push(i);
+  }
+
+  return pse;
+}
+
+vector<int> nextSmallestElem(vector<int> &heights) {
+  stack<int> st;
+  int n = heights.size();
+  vector<int> nse(n);
+  for (int i = n - 1; i >= 0; i--) {
+    while (!st.empty() && heights[i] <= heights[st.top()]) {
+      st.pop();
+    }
+
+    if (!st.empty()) {
+      nse[i] = st.top();
+    } else {
+      nse[i] = n;
+    }
+
+    st.push(i);
+  }
+
+  return nse;
+}
+
+int largestRectangleArea(vector<int> &heights) {
+  vector<int> pse = prevSmallestElem(heights);
+  vector<int> nse = nextSmallestElem(heights);
+  int maxArea = MININT;
+
+  for (int i = 0; i < heights.size(); i++) {
+    maxArea = max(maxArea, heights[i] * (nse[i] - pse[i] - 1));
+  }
+
+  return maxArea;
+}
+
+int main() {
+  vector<int> arr1 = {2, 1, 5, 6, 2, 3};
+  vector<int> arr2 = {2, 4};
+
+  cout << largestRectangleArea(arr1) << endl;
+  cout << largestRectangleArea(arr2) << endl;
+}
