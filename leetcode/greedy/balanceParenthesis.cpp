c6eaacb54544aa4deb348b66f8f0abb3bdfb591c
Souradip Saha <22052939@kiit.ac.in>
2025-09-03 02:52:20 +0530

    soem improvements

--- leetcode/greedy/balanceParenthesis.cpp
+++ leetcode/greedy/balanceParenthesis.cpp
@@ -17,13 +17,22 @@ bool checkValidString(string s) {
     }
 
     if (ch == '*') {
-      if (low > 0) low--; // * = ')'
+      // if low = 0 that means now '(' exists in left side so * might be '' so we keep low as it is
+      // if low > 0 that means '(' at left exists then we do low--
+      if (low > 0) low--; // * = ')' or ''
       high++; // * = '('
     }
 
-    if (high < 0) return false; // Too many ')'
+    if (high < 0) return false; // No of ')' > '('
   }
 
   return low == 0;
 
+}
+
+int main() {
+
+  cout << checkValidString("(*))") << endl;
+  cout << checkValidString("(*)") << endl;
+  cout << checkValidString("())(") << endl; // if (high < 0) return false; is satisfied here 
 }
